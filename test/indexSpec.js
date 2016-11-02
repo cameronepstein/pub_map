23e473fcc5b8b8e0a00f704992a110a1f9e3f8d6
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-11-02 16:11:52 +0000

    add minimal css

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -1,10 +1,22 @@
-'use strict'
+process.env.NODE_ENV = 'test';
+var app = require('../server');
+// use zombie.js as headless browser
+var Browser = require('zombie');
 
-var chai = require('chai');
-var expect = chai.expect;
-
-describe('London button', function() {
-  it('renders a London button', function()  {
-    expect(londonButton.innerHTML).to.equal('London');
+describe('homepage', function() {
+  before(function() {
+    this.server = http.createServer(app).listen(8000);
+    // initialize the browser using the same port as the test application
+    this.browser = new Browser({ site: 'http://localhost:8000' });
   });
+
+  // load the contact page
+  before(function(done) {
+    this.browser.visit('/', done);
+  });
+
+  it('should show contact a form');
+  it('should refuse empty submissions');
+  // ...
+
 });
